0c33525727c24ea6cd64c4e2f4cd5251f566de9e
ivannissimrch <ivannissimrch@gmail.com>
2025-03-12 16:07:25 -0400

    Add scroll functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
+"use client";
+
+import { RefObject, useRef } from "react";
 import Form from "./components/Form";
 
+export interface Inputs {
+  title: string;
+  inputRef: RefObject<HTMLDivElement | null>;
+}
+
 export default function Home() {
-  const inputs = ["Persona", "Context", "Task", "Output", "Constraint"];
+  const personaRef = useRef<HTMLDivElement | null>(null);
+  const contextRef = useRef<HTMLDivElement | null>(null);
+  const taskRef = useRef<HTMLDivElement | null>(null);
+  const outputRef = useRef<HTMLDivElement | null>(null);
+  const constrainRef = useRef<HTMLDivElement | null>(null);
+
+  const inputs: Inputs[] = [
+    { title: "Persona", inputRef: personaRef },
+    { title: "Context", inputRef: contextRef },
+    { title: "Task", inputRef: taskRef },
+    { title: "Output", inputRef: outputRef },
+    { title: "Constrain", inputRef: constrainRef },
+  ];
 
   return (
-    <div className="min-h-full bg-amber-50">
+    <div className="h-full bg-amber-500">
       <header className="bg-white shadow-sm">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">
@@ -12,10 +32,8 @@ export default function Home() {
           </h1>
         </div>
       </header>
-      <main className="bg-blue-200 min-h-screen">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8  ">
-          <Form inputs={inputs} />
-        </div>
+      <main className="bg-amber-950 h ">
+        <Form inputs={inputs} />
       </main>
     </div>
   );
